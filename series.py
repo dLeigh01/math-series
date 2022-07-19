804eb4f1c8c87da5be1b4e7cb44a3d84fe7e7fa5
Dana Huffman <dana.huffman@yahoo.com>
2022-07-19 16:33:12 -0700

    Completed sum_series function

--- series.py
+++ series.py
@@ -21,3 +21,17 @@ def lucas(n):
     if n == 1:
         return 1
     return lucas(n - 1) + lucas(n - 2)
+
+def sum_series(n, a=0, b=1):
+    """
+    Returns the value of the provided number's index in the fibonacci sequence.
+    :param n: (int): a single integer
+        a - (int)(optional): a starting integer for the 0 index
+        b - (int)(optional): a starting integer for the 1 index
+    :return: sum_series(n - 1, a, b) + sum_series(n - 2, a, b) - (int): the lucas or fibonacci sequence value
+    """
+    if n <= 0:
+        return a
+    if n == 1:
+        return b
+    return sum_series(n - 1, a, b) + sum_series(n - 2, a, b)
